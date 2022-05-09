d730d374f73bedbd0093b1d0ed69f0b43519b4a8
Saoussen-CH <saoussen.chaabnia.pro@gmail.com>
2022-05-09 12:07:44 +0100

    add live_api_test.py

---
+++ live_api_test.py
@@ -0,0 +1,80 @@
+import json
+import requests
+
+
+hekoru_endpoint = 'https://udacitymlopsp3cicd.herokuapp.com/'
+
+def test_api_live_get_predictions_inf1():
+    """ Test Fast API predict route with a '<=50K' salary prediction result """
+
+    app_url = hekoru_endpoint + "/predict"
+    
+    expected_res = "Predicts ['<=50K']"
+
+    test_data = {
+        "age": 4,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 2174,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States",
+    }
+    headers = {"Content-Type": "application/json"}
+
+    r = requests.post(app_url, data=json.dumps(test_data), headers=headers)
+    assert r.status_code == 200
+    assert (r.json()["predict"][: len(expected_res)]) == expected_res
+    return r.json()["predict"][: len(expected_res)]
+
+
+def test_api_live_get_predictions_inf2():
+    """ Test Fast API predict route with a '>50K' salary prediction result """
+    app_url = hekoru_endpoint + "/predict"
+
+    expected_res = "Predicts ['>50K']"
+
+    test_data = {
+        "age": 40,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 20000,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States",
+    }
+    headers = {"Content-Type": "application/json"}
+
+    r = requests.post(app_url, data=json.dumps(test_data), headers=headers)
+    assert r.status_code == 200
+    assert (r.json()["predict"][: len(expected_res)]) == expected_res
+    return r.json()["predict"][: len(expected_res)]
+
+
+if __name__ == "__main__":
+
+
+
+
+    # Call live testing function
+    print("test_api_live_get_predictions_inf1 ...")
+    res = test_api_live_get_predictions_inf1()
+    print(res)
+
+    print("test_api_live_get_predictions_inf2 ...")
+    res = test_api_live_get_predictions_inf2()
+    print(res)
--- main.py
+++ main.py
@@ -12,6 +12,14 @@ from pydantic import BaseModel, Field
 from starter.ml.model import *
 from starter.ml.data import *
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    
 # FastAPI instance
 app = FastAPI()
 
