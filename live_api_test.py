65598df0e4848690bd94808719f35be4200fda7a
Saoussen-CH <saoussen.chaabnia.pro@gmail.com>
2022-05-09 12:33:15 +0100

    Add live_api_test.py

--- live_api_test.py
+++ live_api_test.py
@@ -2,34 +2,29 @@ import json
 import requests
 
 
-hekoru_endpoint = 'https://udacitymlopsp3cicd.herokuapp.com/'
+hekoru_endpoint = 'https://mlopsp3.herokuapp.com/'
 
 def test_api_live_get_predictions_inf1():
     """ Test Fast API predict route with a '<=50K' salary prediction result """
 
-    app_url = hekoru_endpoint + "/predict"
-    
-    expected_res = "Predicts ['<=50K']"
 
-    test_data = {
-        "age": 4,
-        "workclass": "State-gov",
-        "fnlgt": 77516,
-        "education": "Bachelors",
-        "education-num": 13,
-        "marital-status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Male",
-        "capital-gain": 2174,
-        "capital-loss": 0,
-        "hours-per-week": 40,
-        "native-country": "United-States",
-    }
-    headers = {"Content-Type": "application/json"}
-
-    r = requests.post(app_url, data=json.dumps(test_data), headers=headers)
+    r = requests.post('https://mlopsp3.herokuapp.com/predict', json={
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education-num": 13,
+                "marital-status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": 2174,
+                "capital-loss": 0,
+                "hours-per-week": 40,
+                "native-country": "United-States"
+            },
+        )
     assert r.status_code == 200
     assert r.json() == {"prediction": {"salary": "<=50K"}}
     return r.json()["prediction"]
@@ -37,28 +32,26 @@ def test_api_live_get_predictions_inf1():
 
 def test_api_live_get_predictions_inf2():
     """ Test Fast API predict route with a '>50K' salary prediction result """
-    app_url = hekoru_endpoint + "/predict"
 
 
-    test_data = {
-        "age": 40,
-        "workclass": "State-gov",
-        "fnlgt": 77516,
-        "education": "Bachelors",
-        "education-num": 13,
-        "marital-status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Male",
-        "capital-gain": 20000,
-        "capital-loss": 0,
-        "hours-per-week": 40,
-        "native-country": "United-States",
-    }
-    headers = {"Content-Type": "application/json"}
 
-    r = requests.post(app_url, data=json.dumps(test_data), headers=headers)
+
+    r = requests.post('https://mlopsp3.herokuapp.com/predict', json={
+                "age": 52,
+                "workclass": "Self-emp-not-inc",
+                "fnlgt": 209642,
+                "education": "HS-grad",
+                "education-num": 9,
+                "marital-status": "Married-civ-spouse",
+                "occupation": "Exec-managerial",
+                "relationship": "Husband",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": 0,
+                "capital-loss": 0,
+                "hours-per-week": 45,
+                "native-country": "United-States"
+            }, )
     assert r.status_code == 200
     assert r.json() == {"prediction": {"salary": ">50K"}}
     return r.json()["prediction"]
